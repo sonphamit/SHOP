1573559e9aee2929c935659deeceb74ec690028f
sonag <sonag@pm.me>
2021-01-14 02:25:14 +0700

    update product form

--- Infrastructure/Models/Identity/RoleModel.cs
+++ Infrastructure/Models/Identity/RoleModel.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Models
 {
     public class RoleModel
     {
-        [Required(ErrorMessage = "* Required")]
+        [Required(ErrorMessage = "Role is required")]
         [StringLength(255, MinimumLength = 3, ErrorMessage = "Name is too short.")]
         public string Name { get; set; }
         public string Id { get; set; }
